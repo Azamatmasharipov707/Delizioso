8c48e5910285a3c742d91c85a89b1c607e22d840
Laylo <bekberganovalaylo97@gmail.com>
2026-04-02 10:51:39 +0500

    nav responsive done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: '15px',
+      screens: {
+        sm: '1230px'
+      }
+    },
     extend: {
       colors: {
         primery: '#FF8A00',
         secondary: '#3FA72F',
         thirdstage: '#311F09'
-      }
+      },
     },
-    container: {
-      center: true,
-      padding: '15px',
-      sm: '1130px'
-    }
   },
   plugins: [],
 }
